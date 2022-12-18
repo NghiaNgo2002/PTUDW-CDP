a7238d90a176e7c4e1ab91181203107457110654
Pal Nguyen <pnpal2002@gmail.com>
2022-12-19 02:10:51 +0700

    feat: paginate product_menu (without filter, sort)

--- db/index.js
+++ db/index.js
@@ -5,13 +5,12 @@ const db = { connection: null };
 
 (async () => {
   db.connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "chien401",
-    database: "netcafe",
+    host: "db4free.net",
+    user: "adminxcdp",
+    password: "12345678",
+    database: "netcafexcdp",
   });
   console.log("Database connected!");
 })();
-
 
 module.exports = db;
