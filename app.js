4b2fc6c62422bdc107c242348a28f7452caa9b26
phuc4570 <phuc4570@gmail.com>
2022-12-21 13:57:55 +0700

    feat: upload image to set avatar. Ajax for check password when changing password. Fix: some error when submit many form in one webpage

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require('express-session');
+const bodyParser= require('body-parser');
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./components/_auth/index");
