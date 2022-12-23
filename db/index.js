78d5419ce825f989d44b38ef92f19fb61ac6c93b
Phúc Nguyễn <phuc4570@gmail.com>
2022-12-23 22:21:49 +0700

    fix: change database

--- db/index.js
+++ db/index.js
@@ -5,9 +5,10 @@ const db = { connection: null };
 
 (async () => {
   db.connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "chien401",
+    host: "mysql-101628-0.cloudclusters.net",
+    port: 10024,
+    user: "admin",
+    password: "34eMcl8t",
     database: "netcafe",
   });
   console.log("Database connected!");
