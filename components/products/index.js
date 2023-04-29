c3358c7c5cdd31f87bc30e9d3007d5573bc0d838
vdkhoi20 <vdkhoi.itai@gmail.com>
2023-04-29 21:20:54 +0700

    Revert "features account password"
    This reverts commit 7ac23259a23c542f38897186896015098cea945d.

--- components/products/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const productController = require('./productController');
-
-router.get('/', productController.details);
-router.get('/edit-table', productController.edit_table);
-
-module.exports = router;
